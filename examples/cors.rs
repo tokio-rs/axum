e63e6772eba39154456891d171f8cd3fba9913b1
heliumbrain <nils@kanevad.se>
2021-08-13 22:18:29 +0200

    CORS example
    A basic example of how to set up CORS response headers

---
+++ examples/cors.rs
@@ -0,0 +1,44 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run --example cors
+//! ```
+
+use axum::prelude::*;
+use http::{HeaderValue, header};
+use tower::ServiceBuilder;
+use std::net::SocketAddr;
+use tower_http::set_header::SetResponseHeaderLayer;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+	// define the CORS-headers you want to add as a Tower service
+	let cors_middleware = ServiceBuilder::new()
+	.layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
+		header::ACCESS_CONTROL_ALLOW_METHODS,
+		HeaderValue::from_static("OPTION, GET, POST, PATCH, DELETE"),
+	))
+	.layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
+		header::ACCESS_CONTROL_ALLOW_ORIGIN,
+		HeaderValue::from_static("*"),
+	))
+	.into_inner();
+
+    // build our application with a route and add the layer off middleware
+    let app = route("/", get(handler))
+	.layer(cors_middleware);
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> response::Html<&'static str> {
+    response::Html("<h1>Hello, World!</h1>")
+}
