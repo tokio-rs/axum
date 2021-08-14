01d8a9fa039fe5caa427d1659645e98aa3232fc0
heliumbrain <nils@kanevad.se>
2021-08-14 11:38:59 +0200

    Fixed formatting...

--- examples/cors.rs
+++ examples/cors.rs
@@ -5,30 +5,29 @@
 //! ```
 
 use axum::prelude::*;
-use http::{HeaderValue, header};
-use tower::ServiceBuilder;
+use http::{header, HeaderValue};
 use std::net::SocketAddr;
+use tower::ServiceBuilder;
 use tower_http::set_header::SetResponseHeaderLayer;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-	// define the CORS-headers you want to add as a Tower service
-	let cors_middleware = ServiceBuilder::new()
-	.layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
-		header::ACCESS_CONTROL_ALLOW_METHODS,
-		HeaderValue::from_static("OPTION, GET, POST, PATCH, DELETE"),
-	))
-	.layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
-		header::ACCESS_CONTROL_ALLOW_ORIGIN,
-		HeaderValue::from_static("*"),
-	))
-	.into_inner();
+    // define the CORS-headers you want to add as a Tower service
+    let cors_middleware = ServiceBuilder::new()
+        .layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
+            header::ACCESS_CONTROL_ALLOW_METHODS,
+            HeaderValue::from_static("OPTION, GET, POST, PATCH, DELETE"),
+        ))
+        .layer(SetResponseHeaderLayer::<_, Body>::if_not_present(
+            header::ACCESS_CONTROL_ALLOW_ORIGIN,
+            HeaderValue::from_static("*"),
+        ))
+        .into_inner();
 
     // create application with a route and add the layer off middleware including a handler for OPTIONS requests.
-    let app = route("/", get(handler).options(|| async { "" }))
-	.layer(cors_middleware);
+    let app = route("/", get(handler).options(|| async { "" })).layer(cors_middleware);
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
