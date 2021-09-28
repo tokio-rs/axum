c8c20e1aaeae96d9e1e8d03f36bd0bfeacec8520
liteng <yujinjianxin@gmail.com>
2021-09-28 18:54:03 +0800

    Add validator example

---
+++ examples/validator/src/main.rs
@@ -0,0 +1,78 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-validator
+//!
+//! curl '127.0.0.1:3000?name='
+//! -> Input validation error: [name: Can not be empty]
+//!
+//! curl '127.0.0.1:3000?name=LT'
+//! -> <h1>Hello, LT!</h1>
+//! ```
+
+use axum::{
+    body::{Bytes, Full},
+    extract::Form,
+    handler::get,
+    http::{Response, StatusCode},
+    response::{Html, IntoResponse},
+    Router,
+};
+use serde::Deserialize;
+use std::{convert::Infallible, net::SocketAddr};
+use thiserror::Error;
+use validator::Validate;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct NameInput {
+    #[validate(length(min = 1, message = "Can not be empty"))]
+    pub name: String,
+}
+
+async fn handler(Form(input): Form<NameInput>) -> Result<Html<String>, ServerError> {
+    input.validate()?;
+
+    Ok(Html(format!("<h1>Hello, {}!</h1>", input.name)))
+}
+
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error(transparent)]
+    ValidationError(#[from] validator::ValidationErrors),
+
+    #[error(transparent)]
+    InternalServerError(#[from] anyhow::Error),
+}
+
+impl IntoResponse for ServerError {
+    type Body = Full<Bytes>;
+    type BodyError = Infallible;
+
+    fn into_response(self) -> Response<Self::Body> {
+        match self {
+            ServerError::ValidationError(_) => {
+                let message = format!("Input validation error: [{}]", self).replace("\n", ", ");
+                (StatusCode::BAD_REQUEST, message)
+            }
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                StatusCode::INTERNAL_SERVER_ERROR.to_string(),
+            ),
+        }
+        .into_response()
+    }
+}
