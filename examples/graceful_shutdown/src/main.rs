0b3ee5b2cece4d613655af1bdab3b48f2582af98
LT <yujinjianxin@gmail.com>
2021-09-28 17:08:49 +0800

    Add graceful shutdown example (#349)

---
+++ examples/graceful_shutdown/src/main.rs
@@ -0,0 +1,55 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-graceful-shutdown
+//! kill or ctrl-c
+//! ```
+
+use axum::{handler::get, response::Html, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+#[cfg(unix)]
+pub async fn shutdown_signal() {
+    use std::io;
+    use tokio::signal::unix::SignalKind;
+
+    async fn terminate() -> io::Result<()> {
+        tokio::signal::unix::signal(SignalKind::terminate())?
+            .recv()
+            .await;
+        Ok(())
+    }
+
+    tokio::select! {
+        _ = terminate() => {},
+        _ = tokio::signal::ctrl_c() => {},
+    }
+    println!("signal received, starting graceful shutdown")
+}
+
+#[cfg(windows)]
+pub async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("faild to install CTRL+C handler");
+    println!("signal received, starting graceful shutdown")
+}
