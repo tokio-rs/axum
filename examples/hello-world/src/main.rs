a048d6443bec4d00ecd345a2e44d1017bc8dec51
David Pedersen <david.pdrsn@gmail.com>
2021-11-02 13:07:34 +0100

    Revert hello-world example
    Was accidentally changed in another PR

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -4,58 +4,23 @@
 //! cargo run -p example-hello-world
 //! ```
 
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
 #[tokio::main]
 async fn main() {
-    use axum::async_trait;
-    use axum::http::StatusCode;
-    use axum::{
-        extract::{extractor_middleware, FromRequest, RequestParts},
-        routing::{get, post},
-        Router,
-    };
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
 
-    // An extractor that performs authorization.
-    struct RequireAuth;
-
-    #[async_trait]
-    impl<B> FromRequest<B> for RequireAuth
-    where
-        B: Send,
-    {
-        type Rejection = StatusCode;
-
-        async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-            let auth_header = req
-                .headers()
-                .and_then(|headers| headers.get(axum::http::header::AUTHORIZATION))
-                .and_then(|value| value.to_str().ok());
-
-            if let Some(value) = auth_header {
-                if value == "secret" {
-                    return Ok(Self);
-                }
-            }
-
-            Err(StatusCode::UNAUTHORIZED)
-        }
-    }
-
-    async fn handler() {
-        // If we get here the request has been authorized
-    }
-
-    async fn other_handler() {
-        // If we get here the request has been authorized
-    }
-
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/foo", post(other_handler))
-        // The extractor will run before all routes
-        .layer(extractor_middleware::<RequireAuth>());
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
