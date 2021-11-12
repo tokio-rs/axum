ca48ac4b1e3953feb4af112110b789d60fc6f263
David Pedersen <david.pdrsn@gmail.com>
2021-11-12 19:05:39 +0100

    More clearly document accepting multiple methods
    Seen a few users ask about this and could be documented more clearly.

--- axum/src/lib.rs
+++ axum/src/lib.rs
@@ -69,12 +69,13 @@
 //! // our router
 //! let app = Router::new()
 //!     .route("/", get(root))
-//!     .route("/foo", get(foo))
+//!     .route("/foo", get(get_foo).post(post_foo))
 //!     .route("/foo/bar", get(foo_bar));
 //!
 //! // which calls one of these handlers
 //! async fn root() {}
-//! async fn foo() {}
+//! async fn get_foo() {}
+//! async fn post_foo() {}
 //! async fn foo_bar() {}
 //! # async {
 //! # axum::Server::bind(&"".parse().unwrap()).serve(app.into_make_service()).await.unwrap();
