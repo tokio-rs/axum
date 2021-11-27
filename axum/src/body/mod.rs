d601af7c11bdf9e1511d75eb6be4667b0d29b568
KaiJewson <kai.jewson@gmail.com>
2021-11-27 06:27:03 +0000

    Extract `try_downcast` to `util` module

--- axum/src/body/mod.rs
+++ axum/src/body/mod.rs
@@ -1,7 +1,6 @@
 //! HTTP body utilities.
 
-use crate::{BoxError, Error};
-use std::any::Any;
+use crate::{util::try_downcast, BoxError, Error};
 
 mod stream_body;
 
@@ -23,31 +22,12 @@ pub use bytes::Bytes;
 pub type BoxBody = http_body::combinators::UnsyncBoxBody<Bytes, Error>;
 
 /// Convert a [`http_body::Body`] into a [`BoxBody`].
-///
-/// If the given body is already a [`BoxBody`], this function will not box the body again.
 pub fn boxed<B>(body: B) -> BoxBody
 where
     B: http_body::Body<Data = Bytes> + Send + 'static,
     B::Error: Into<BoxError>,
 {
-    downcast_box_body(body).unwrap_or_else(|body| body.map_err(Error::new).boxed_unsync())
-}
-
-fn downcast_box_body<B: 'static>(body: B) -> Result<BoxBody, B> {
-    let mut body = Some(body);
-    if let Some(body) = <dyn Any>::downcast_mut::<Option<BoxBody>>(&mut body) {
-        Ok(body.take().unwrap())
-    } else {
-        Err(body.unwrap())
-    }
-}
-
-#[test]
-fn body_not_double_boxed() {
-    let body = Full::new(Bytes::from("hello world"));
-    assert!(downcast_box_body(body.clone()).is_err());
-    assert!(downcast_box_body(boxed(body.clone())).is_ok());
-    assert!(downcast_box_body(boxed(boxed(body.clone()))).is_ok());
+    try_downcast(body).unwrap_or_else(|body| body.map_err(Error::new).boxed_unsync())
 }
 
 pub(crate) fn empty() -> BoxBody {
