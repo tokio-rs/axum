ecf33599803f4540db8b331e6b20e0acd04e8e8c
David Pedersen <david.pdrsn@gmail.com>
2021-12-12 17:21:29 +0100

    Fix misc warnings from CI (#620)

--- examples/async-graphql/src/starwars/model.rs
+++ examples/async-graphql/src/starwars/model.rs
@@ -122,12 +122,7 @@ impl QueryRoot {
         first: Option<i32>,
         last: Option<i32>,
     ) -> FieldResult<Connection<usize, Human, EmptyFields, EmptyFields>> {
-        let humans = ctx
-            .data_unchecked::<StarWars>()
-            .humans()
-            .iter()
-            .copied()
-            .collect::<Vec<_>>();
+        let humans = ctx.data_unchecked::<StarWars>().humans().to_vec();
         query_characters(after, before, first, last, &humans)
             .await
             .map(|conn| conn.map_node(Human))
@@ -149,12 +144,7 @@ impl QueryRoot {
         first: Option<i32>,
         last: Option<i32>,
     ) -> FieldResult<Connection<usize, Droid, EmptyFields, EmptyFields>> {
-        let droids = ctx
-            .data_unchecked::<StarWars>()
-            .droids()
-            .iter()
-            .copied()
-            .collect::<Vec<_>>();
+        let droids = ctx.data_unchecked::<StarWars>().droids().to_vec();
         query_characters(after, before, first, last, &droids)
             .await
             .map(|conn| conn.map_node(Droid))
