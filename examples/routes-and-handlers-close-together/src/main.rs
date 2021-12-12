ba42783df286a673b490d1fa74f84a45e3072f94
David Pedersen <david.pdrsn@gmail.com>
2021-12-12 23:54:18 +0100

    Example for defining routes and handlers close together (#621)

---
+++ examples/routes-and-handlers-close-together/src/main.rs
@@ -0,0 +1,54 @@
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-routes-and-handlers-close-together
+//! ```
+
+use axum::{
+    routing::{get, post, MethodRouter},
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .merge(root())
+        .merge(get_foo())
+        .merge(post_foo());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn root() -> Router {
+    async fn handler() -> &'static str {
+        "Hello, World!"
+    }
+
+    route("/", get(handler))
+}
+
+fn get_foo() -> Router {
+    async fn handler() -> &'static str {
+        "Hi from `GET /foo`"
+    }
+
+    route("/foo", get(handler))
+}
+
+fn post_foo() -> Router {
+    async fn handler() -> &'static str {
+        "Hi from `POST /foo`"
+    }
+
+    route("/foo", post(handler))
+}
+
+fn route(path: &str, method_router: MethodRouter) -> Router {
+    Router::new().route(path, method_router)
+}
