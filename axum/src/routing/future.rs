c135436cc97644bb46b6509cf50d85abe49234ac
David Pedersen <david.pdrsn@gmail.com>
2022-02-14 10:56:08 +0100

    Fix setting `content-length: 0` for HEAD responses with compression middleware (#755)
    * Fix getting `content-length` for `chunked` responses
    
    Fixes #747
    
    * changelog
    
    * Fix `cargo deny bans`
    
    https://github.com/tokio-rs/axum/pull/753 will fix things properly

--- axum/src/routing/future.rs
+++ axum/src/routing/future.rs
@@ -1,21 +1,3 @@
 //! Future types.
 
-use crate::response::Response;
-use std::convert::Infallible;
-
 pub use super::{into_make_service::IntoMakeServiceFuture, route::RouteFuture};
-
-opaque_future! {
-    /// Response future for [`Router`](super::Router).
-    pub type RouterFuture<B> = RouteFuture<B, Infallible>;
-}
-
-impl<B> RouterFuture<B> {
-    pub(super) fn from_future(future: RouteFuture<B, Infallible>) -> Self {
-        Self::new(future)
-    }
-
-    pub(super) fn from_response(response: Response) -> Self {
-        Self::new(RouteFuture::from_response(response))
-    }
-}
--- axum/src/routing/tests/mod.rs
+++ axum/src/routing/tests/mod.rs
@@ -645,3 +645,36 @@ async fn head_content_length_through_hyper_server_that_hits_fallback() {
     let res = client.head("/").send().await;
     assert_eq!(res.headers()["content-length"], "3");
 }
+
+#[tokio::test]
+async fn head_with_middleware_applied() {
+    use tower_http::compression::{predicate::SizeAbove, CompressionLayer};
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(CompressionLayer::new().compress_when(SizeAbove::new(0)));
+
+    let client = TestClient::new(app);
+
+    // send GET request
+    let res = client
+        .get("/")
+        .header("accept-encoding", "gzip")
+        .send()
+        .await;
+    assert_eq!(res.headers()["transfer-encoding"], "chunked");
+    // cannot have `transfer-encoding: chunked` and `content-length`
+    assert!(!res.headers().contains_key("content-length"));
+
+    // send HEAD request
+    let res = client
+        .head("/")
+        .header("accept-encoding", "gzip")
+        .send()
+        .await;
+    // no response body so no `transfer-encoding`
+    assert!(!res.headers().contains_key("transfer-encoding"));
+    // no content-length since we cannot know it since the response
+    // is compressed
+    assert!(!res.headers().contains_key("content-length"));
+}
