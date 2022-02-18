4819d000b4577865f2a08bbd3ae5df9ca8de965f
David Pedersen <david.pdrsn@gmail.com>
2022-02-18 13:48:53 +0100

    Update axum-extra/src/routing/typed.rs
    Co-authored-by: Jonas Platte <jplatte@users.noreply.github.com>

--- axum-extra/src/routing/typed.rs
+++ axum-extra/src/routing/typed.rs
@@ -119,7 +119,7 @@ use super::sealed::Sealed;
 ///
 /// ## Percent encoding
 ///
-/// The generated [`Display`] implementation will automatically percent encoding parameters:
+/// The generated [`Display`] implementation will automatically percent-encode parameters:
 ///
 /// ```
 /// use serde::Deserialize;
