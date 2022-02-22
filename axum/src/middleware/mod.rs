0b987c45c726de062af8ec42e9c52f1c72ecf8fe
David Pedersen <david.pdrsn@gmail.com>
2022-02-22 10:54:47 +0100

    Re-export `middleware::from_fn` from axum-extra to avoid making breaking changes

--- axum/src/middleware/mod.rs
+++ axum/src/middleware/mod.rs
@@ -3,3 +3,9 @@
 mod from_fn;
 
 pub use self::from_fn::{from_fn, FromFn, FromFnLayer, Next};
+
+pub mod futures {
+    //! Future types.
+
+    pub use super::from_fn::ResponseFuture as FromFnResponseFuture;
+}
