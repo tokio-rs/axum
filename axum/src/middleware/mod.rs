e29bd87ee7dd38f1e3b4dc3b77e4482282dce48a
David Pedersen <david.pdrsn@gmail.com>
2022-02-22 12:30:10 +0100

    Make `middleware::from_fn` response future public

--- axum/src/middleware/mod.rs
+++ axum/src/middleware/mod.rs
@@ -5,3 +5,9 @@
 mod from_fn;
 
 pub use self::from_fn::{from_fn, FromFn, FromFnLayer, Next};
+
+pub mod futures {
+    //! Future types.
+
+    pub use super::from_fn::ResponseFuture as FromFnResponseFuture;
+}
