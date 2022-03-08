08d1473531d9b47ba969fc2d14f7cdd8bdf97cd5
David Pedersen <david.pdrsn@gmail.com>
2022-03-08 23:04:44 +0100

    clean up and more comments

--- axum/src/routing/strip_prefix.rs
+++ axum/src/routing/strip_prefix.rs
@@ -42,63 +42,78 @@ where
 }
 
 fn strip_prefix(uri: &Uri, prefix: &str) -> Option<Uri> {
-    let path_and_query = if let Some(path_and_query) = uri.path_and_query() {
-        // Check whether the prefix matches the path and if so how long the matching prefix is.
-        //
-        // # Examples
-        //
-        // prefix = /api
-        // uri    = /api/users
-        //          ^^^^ this much is matched and the length is 4. Thus if we chop off the first 4
-        //          characters we get the remainder
-        //
-        // prefix = /api/:version
-        // uri    = /api/v0/users
-        //          ^^^^^^^ this much is matched and the length is 7.
-        let mut matching_prefix_length = Some(0);
-        for item in zip_longest(segments(path_and_query.path()), segments(prefix)) {
-            // count the `/`
-            *matching_prefix_length.as_mut().unwrap() += 1;
+    let path_and_query = uri.path_and_query()?;
 
-            match item {
-                Item::Both(path_segment, prefix_segment) => {
-                    if prefix_segment.starts_with(':') || path_segment == prefix_segment {
-                        *matching_prefix_length.as_mut().unwrap() += path_segment.len();
-                    } else if prefix_segment.is_empty() {
-                        break;
-                    } else {
-                        matching_prefix_length = None;
-                        break;
-                    }
-                }
-                Item::First(_) => {
+    // Check whether the prefix matches the path and if so how long the matching prefix is.
+    //
+    // For example:
+    //
+    // prefix = /api
+    // path   = /api/users
+    //          ^^^^ this much is matched and the length is 4. Thus if we chop off the first 4
+    //          characters we get the remainder
+    //
+    // prefix = /api/:version
+    // path   = /api/v0/users
+    //          ^^^^^^^ this much is matched and the length is 7.
+    let mut matching_prefix_length = Some(0);
+    for item in zip_longest(segments(path_and_query.path()), segments(prefix)) {
+        // count the `/`
+        *matching_prefix_length.as_mut().unwrap() += 1;
+
+        match item {
+            Item::Both(path_segment, prefix_segment) => {
+                if prefix_segment.starts_with(':') || path_segment == prefix_segment {
+                    // the prefix segment is either a param, which matches anything, or
+                    // it actually matches the path segment
+                    *matching_prefix_length.as_mut().unwrap() += path_segment.len();
+                } else if prefix_segment.is_empty() {
+                    // the prefix ended in a `/` so we got a match.
+                    //
+                    // For example:
+                    //
+                    // prefix = /foo/
+                    // path   = /foo/bar
+                    //
+                    // The prefix matches and the new path should be `/bar`
                     break;
-                }
-                Item::Second(_) => {
+                } else {
+                    // the prefix segment didn't match so there is no match
                     matching_prefix_length = None;
                     break;
                 }
             }
+            // the path had more segments than the prefix but we got a match.
+            //
+            // For example:
+            //
+            // prefix = /foo
+            // path   = /foo/bar
+            Item::First(_) => {
+                break;
+            }
+            // the prefix had more segments than the path so there is no match
+            Item::Second(_) => {
+                matching_prefix_length = None;
+                break;
+            }
         }
+    }
 
-        let after_prefix = if let Some(idx) = matching_prefix_length {
-            uri.path().split_at(idx).1
-        } else {
-            return None;
-        };
+    // if the prefix matches it will always do so up until a `/`, it cannot match only
+    // part of a segment. Therefore this will always be at a char boundary and `split_at` wont
+    // panic
+    let after_prefix = uri.path().split_at(matching_prefix_length?).1;
 
-        match (after_prefix.starts_with('/'), path_and_query.query()) {
-            (true, None) => after_prefix.parse().unwrap(),
-            (true, Some(query)) => format!("{}?{}", after_prefix, query).parse().unwrap(),
-            (false, None) => format!("/{}", after_prefix).parse().unwrap(),
-            (false, Some(query)) => format!("/{}?{}", after_prefix, query).parse().unwrap(),
-        }
-    } else {
-        return None;
+    let new_path_and_query = match (after_prefix.starts_with('/'), path_and_query.query()) {
+        (true, None) => after_prefix.parse().unwrap(),
+        (true, Some(query)) => format!("{}?{}", after_prefix, query).parse().unwrap(),
+        (false, None) => format!("/{}", after_prefix).parse().unwrap(),
+        (false, Some(query)) => format!("/{}?{}", after_prefix, query).parse().unwrap(),
     };
 
     let mut parts = uri.clone().into_parts();
-    parts.path_and_query = Some(path_and_query);
+    parts.path_and_query = Some(new_path_and_query);
 
     Some(Uri::from_parts(parts).unwrap())
 }
