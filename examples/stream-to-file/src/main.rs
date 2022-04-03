37f0ad21d48b6ee1f662b9678a73e0157d2742c6
David Pedersen <david.pdrsn@gmail.com>
2022-04-03 12:13:23 +0200

    add title tag

--- examples/stream-to-file/src/main.rs
+++ examples/stream-to-file/src/main.rs
@@ -57,7 +57,9 @@ async fn show_form() -> Html<&'static str> {
         r#"
         <!doctype html>
         <html>
-            <head></head>
+            <head>
+                <title>Upload something!</title>
+            </head>
             <body>
                 <form action="/" method="post" enctype="multipart/form-data">
                     <div>
