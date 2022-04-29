6e1835074c1dd3e1beb631c87cf0096c8624fa6f
Marcus Griep <marcus@griep.us>
2022-04-29 11:52:21 -0400

    Implement `IntoResponse` for `Response<()>` and `response::Parts` (#950)
    * feat(axum-core): add IntoResponse for `http::response` types
    
    * chore: narrow impl to `Response<()>`
    
    * include extensions
    
    * changelog
    
    Co-authored-by: David Pedersen <david.pdrsn@gmail.com>

--- axum-core/src/response/into_response.rs
+++ axum-core/src/response/into_response.rs
@@ -404,6 +404,27 @@ where
     }
 }
 
+impl<R> IntoResponse for (http::response::Parts, R)
+where
+    R: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        let (parts, res) = self;
+        (parts.status, parts.headers, parts.extensions, res).into_response()
+    }
+}
+
+impl<R> IntoResponse for (http::response::Response<()>, R)
+where
+    R: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        let (template, res) = self;
+        let (parts, ()) = template.into_parts();
+        (parts, res).into_response()
+    }
+}
+
 macro_rules! impl_into_response {
     ( $($ty:ident),* $(,)? ) => {
         #[allow(non_snake_case)]
