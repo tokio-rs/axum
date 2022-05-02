9e09b90b836364fbefd93ac4fcaf4b6092f9d881
David Pedersen <david.pdrsn@gmail.com>
2022-05-02 17:36:43 +0200

    fix nitpicks

--- examples/rest-grpc-multiplex/src/main.rs
+++ examples/rest-grpc-multiplex/src/main.rs
@@ -1,15 +1,22 @@
-mod multiplex_service;
+//! Run with
+//!
+//! ```not_rust
+//! cd examples && cargo run -p example-rest-grpc-multiplex
+//! ```
 
+use self::multiplex_service::MultiplexService;
+use axum::{routing::get, Router};
+use proto::{
+    greeter_server::{Greeter, GreeterServer},
+    HelloReply, HelloRequest,
+};
 use std::net::SocketAddr;
-
-use rpc_helloworld::{greeter_server::Greeter, HelloReply, HelloRequest};
 use tonic::{Response as TonicResponse, Status};
-
-use axum::routing::get;
-
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-mod rpc_helloworld {
+mod multiplex_service;
+
+mod proto {
     tonic::include_proto!("helloworld");
 }
 
@@ -22,11 +29,12 @@ impl Greeter for GrpcServiceImpl {
         &self,
         request: tonic::Request<HelloRequest>,
     ) -> Result<TonicResponse<HelloReply>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
+        tracing::info!("Got a request from {:?}", request.remote_addr());
 
         let reply = HelloReply {
             message: format!("Hello {}!", request.into_inner().name),
         };
+
         Ok(TonicResponse::new(reply))
     }
 }
@@ -47,17 +55,17 @@ async fn main() {
         .init();
 
     // build the rest service
-    let rest = axum::Router::new().route("/", get(web_root).post(web_root));
+    let rest = Router::new().route("/", get(web_root));
 
     // build the grpc service
-    let grpc = rpc_helloworld::greeter_server::GreeterServer::new(GrpcServiceImpl::default());
+    let grpc = GreeterServer::new(GrpcServiceImpl::default());
 
-    let service = multiplex_service::MultiplexService { rest, grpc };
+    // combine them into one service
+    let service = MultiplexService::new(rest, grpc);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
-    hyper::Server::bind(&addr)
-        .tcp_keepalive(Some(std::time::Duration::from_secs(360)))
+    axum::Server::bind(&addr)
         .serve(tower::make::Shared::new(service))
         .await
         .unwrap();
--- examples/rest-grpc-multiplex/src/multiplex_service.rs
+++ examples/rest-grpc-multiplex/src/multiplex_service.rs
@@ -1,17 +1,29 @@
-use std::{convert::Infallible, task::Poll};
-
 use axum::{body::BoxBody, response::IntoResponse};
-use futures::{
-    future::{BoxFuture, Either},
-    ready, TryFutureExt,
-};
+use futures::{future::BoxFuture, ready};
 use hyper::{Body, Request, Response};
+use std::{
+    convert::Infallible,
+    task::{Context, Poll},
+};
 use tower::Service;
 
 #[derive(Clone)]
 pub struct MultiplexService<A, B> {
-    pub rest: A,
-    pub grpc: B,
+    rest: A,
+    rest_ready: bool,
+    grpc: B,
+    grpc_ready: bool,
+}
+
+impl<A, B> MultiplexService<A, B> {
+    pub fn new(rest: A, grpc: B) -> Self {
+        Self {
+            rest,
+            rest_ready: false,
+            grpc,
+            grpc_ready: false,
+        }
+    }
 }
 
 impl<A, B> Service<Request<Body>> for MultiplexService<A, B>
@@ -23,33 +35,51 @@ where
     B::Response: IntoResponse,
     B::Future: Send + 'static,
 {
+    type Response = Response<BoxBody>;
     type Error = Infallible;
     type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-    type Response = Response<BoxBody>;
 
-    fn poll_ready(
-        &mut self,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
-        Poll::Ready(if let Err(err) = ready!(self.rest.poll_ready(cx)) {
-            Err(err)
-        } else {
-            ready!(self.rest.poll_ready(cx))
-        })
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        loop {
+            match (self.rest_ready, self.grpc_ready) {
+                (true, true) => {
+                    return Ok(()).into();
+                }
+                (false, _) => {
+                    ready!(self.rest.poll_ready(cx))?;
+                    self.rest_ready = false;
+                }
+                (_, false) => {
+                    ready!(self.grpc.poll_ready(cx))?;
+                    self.grpc_ready = true;
+                }
+            }
+        }
     }
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
-        let hv = req.headers().get("content-type").map(|x| x.as_bytes());
-
-        let fut = if hv
-            .filter(|value| value.starts_with(b"application/grpc"))
-            .is_some()
-        {
-            Either::Left(self.grpc.call(req).map_ok(|res| res.into_response()))
+        if is_grpc_request(&req) {
+            self.grpc_ready = false;
+            let future = self.grpc.call(req);
+            Box::pin(async move {
+                let res = future.await?;
+                Ok(res.into_response())
+            })
         } else {
-            Either::Right(self.rest.call(req).map_ok(|res| res.into_response()))
-        };
-
-        Box::pin(fut)
+            self.rest_ready = false;
+            let future = self.rest.call(req);
+            Box::pin(async move {
+                let res = future.await?;
+                Ok(res.into_response())
+            })
+        }
     }
+}
+
+fn is_grpc_request<B>(req: &Request<B>) -> bool {
+    req.headers()
+        .get("content-type")
+        .map(|content_type| content_type.as_bytes())
+        .filter(|content_type| content_type.starts_with(b"application/grpc"))
+        .is_some()
 }
