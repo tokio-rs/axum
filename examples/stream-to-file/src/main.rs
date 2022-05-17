9d4e5ce2e9b83e2fa9fb861475b160b1f4d28ae6
David Pedersen <david.pdrsn@gmail.com>
2022-05-17 11:38:39 +0200

    Fix vulnerability in example-stream-to-file example

--- examples/stream-to-file/src/main.rs
+++ examples/stream-to-file/src/main.rs
@@ -46,9 +46,7 @@ async fn save_request_body(
     Path(file_name): Path<String>,
     body: BodyStream,
 ) -> Result<(), (StatusCode, String)> {
-    stream_to_file(&file_name, body)
-        .await
-        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+    stream_to_file(&file_name, body).await
 }
 
 // Handler that returns HTML for a multipart form.
@@ -88,30 +86,51 @@ async fn accept_form(mut multipart: Multipart) -> Result<Redirect, (StatusCode, 
             continue;
         };
 
-        stream_to_file(&file_name, field)
-            .await
-            .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))?;
+        stream_to_file(&file_name, field).await?;
     }
 
     Ok(Redirect::to("/"))
 }
 
 // Save a `Stream` to a file
-async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), io::Error>
+async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
 where
     S: Stream<Item = Result<Bytes, E>>,
     E: Into<BoxError>,
 {
-    // Convert the stream into an `AsyncRead`.
-    let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
-    let body_reader = StreamReader::new(body_with_io_error);
-    futures::pin_mut!(body_reader);
+    if !path_is_valid(path) {
+        return Err((StatusCode::BAD_REQUEST, "Invalid path".to_owned()));
+    }
 
-    // Create the file. `File` implements `AsyncWrite`.
-    let mut file = BufWriter::new(File::create(path).await?);
+    async {
+        // Convert the stream into an `AsyncRead`.
+        let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
+        let body_reader = StreamReader::new(body_with_io_error);
+        futures::pin_mut!(body_reader);
 
-    // Copy the body into the file.
-    tokio::io::copy(&mut body_reader, &mut file).await?;
+        // Create the file. `File` implements `AsyncWrite`.
+        let mut file = BufWriter::new(File::create(path).await?);
 
-    Ok(())
+        // Copy the body into the file.
+        tokio::io::copy(&mut body_reader, &mut file).await?;
+
+        Ok::<_, io::Error>(())
+    }
+    .await
+    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+}
+
+// to prevent directory traversal attacks we ensure the path conists of exactly one normal
+// component
+fn path_is_valid(path: &str) -> bool {
+    let path_decoded = std::path::Path::new(&*path);
+    let mut components = path_decoded.components().peekable();
+
+    if let Some(first) = components.peek() {
+        if !matches!(first, std::path::Component::Normal(_)) {
+            return false;
+        }
+    }
+
+    components.count() == 1
 }
