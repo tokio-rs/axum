8872324bb17f122238e387f49a2c0ad933592b74
David Pedersen <david.pdrsn@gmail.com>
2022-06-15 12:58:12 +0200

    Update examples/low-level-rustls/src/main.rs
    Co-authored-by: Jonas Platte <jplatte+git@posteo.de>

--- examples/low-level-rustls/src/main.rs
+++ examples/low-level-rustls/src/main.rs
@@ -37,7 +37,7 @@ async fn main() {
         .init();
 
     let rustls_config = rustls_server_config(
-        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("self_signed_certs/key.pem"),
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("self_signed_certs").join("key.pem"),
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("self_signed_certs/cert.pem"),
     );
 
