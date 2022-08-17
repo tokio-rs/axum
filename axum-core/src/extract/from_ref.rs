d10547e7e571d54f59d6db20afac66cede8e00a2
David Pedersen <david.pdrsn@gmail.com>
2022-08-17 16:31:06 +0200

    fix docs link

--- axum-core/src/extract/from_ref.rs
+++ axum-core/src/extract/from_ref.rs
@@ -5,7 +5,7 @@
 ///
 /// See [`State`] for more details on how library authors should use this trait.
 ///
-/// [`State`]: axum::extract::State
+/// [`State`]: https://docs.rs/axum/0.6/axum/extract/struct.State.html
 // NOTE: This trait is defined in axum-core, even though it is mainly used with `State` which is
 // defined in axum. That allows crate authors to use it when implementing extractors.
 pub trait FromRef<T> {
