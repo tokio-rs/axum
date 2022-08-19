9bb85c8cf7b8ce7cb5df4153a0d74e38b598f5f9
David Pedersen <david.pdrsn@gmail.com>
2022-08-19 11:35:59 +0200

    comment out tuples for now

--- axum-core/src/extract/tuple.rs
+++ axum-core/src/extract/tuple.rs
@@ -1,75 +1,78 @@
-use super::{FromRequest, FromRequestParts};
-use crate::response::{IntoResponse, Response};
-use async_trait::async_trait;
-use http::request::{Parts, Request};
-use std::convert::Infallible;
+// TODO: Bring this back, and make sure it works with all the different possible permutations
+// See https://github.com/tokio-rs/axum/pull/1277#issuecomment-1220358420 for details
 
-#[async_trait]
-impl<S> FromRequestParts<S> for ()
-where
-    S: Send + Sync,
-{
-    type Rejection = Infallible;
+// use super::{FromRequest, FromRequestParts};
+// use crate::response::{IntoResponse, Response};
+// use async_trait::async_trait;
+// use http::request::{Parts, Request};
+// use std::convert::Infallible;
 
-    async fn from_request_parts(_: &mut Parts, _: &S) -> Result<(), Self::Rejection> {
-        Ok(())
-    }
-}
+// #[async_trait]
+// impl<S> FromRequestParts<S> for ()
+// where
+//     S: Send + Sync,
+// {
+//     type Rejection = Infallible;
 
-macro_rules! impl_from_request {
-    (
-        [$($ty:ident),*], $last:ident
-    ) => {
-        #[async_trait]
-        #[allow(non_snake_case, unused_mut, unused_variables)]
-        impl<S, B, $($ty,)* $last> FromRequest<S, B> for ($($ty,)* $last,)
-        where
-            $( $ty: FromRequestParts<S> + Send, )*
-            $last: FromRequest<S, B> + Send,
-            B: Send + 'static,
-            S: Send + Sync,
-        {
-            type Rejection = Response;
+//     async fn from_request_parts(_: &mut Parts, _: &S) -> Result<(), Self::Rejection> {
+//         Ok(())
+//     }
+// }
 
-            async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
-                let (mut parts, body) = req.into_parts();
+// macro_rules! impl_from_request {
+//     (
+//         [$($ty:ident),*], $last:ident
+//     ) => {
+//         #[async_trait]
+//         #[allow(non_snake_case, unused_mut, unused_variables)]
+//         impl<S, B, $($ty,)* $last> FromRequest<S, B> for ($($ty,)* $last,)
+//         where
+//             $( $ty: FromRequestParts<S> + Send, )*
+//             $last: FromRequest<S, B> + Send,
+//             B: Send + 'static,
+//             S: Send + Sync,
+//         {
+//             type Rejection = Response;
 
-                $(
-                    let $ty = $ty::from_request_parts(&mut parts, state).await.map_err(|err| err.into_response())?;
-                )*
+//             async fn from_request(req: Request<B>, state: &S) -> Result<Self, Self::Rejection> {
+//                 let (mut parts, body) = req.into_parts();
 
-                let req = Request::from_parts(parts, body);
+//                 $(
+//                     let $ty = $ty::from_request_parts(&mut parts, state).await.map_err(|err| err.into_response())?;
+//                 )*
 
-                let $last = $last::from_request(req, state).await.map_err(|err| err.into_response())?;
+//                 let req = Request::from_parts(parts, body);
 
-                Ok(($($ty,)* $last,))
-            }
-        }
-    };
-}
+//                 let $last = $last::from_request(req, state).await.map_err(|err| err.into_response())?;
 
-impl_from_request!([], T1);
-impl_from_request!([T1], T2);
-impl_from_request!([T1, T2], T3);
-impl_from_request!([T1, T2, T3], T4);
-impl_from_request!([T1, T2, T3, T4], T5);
-impl_from_request!([T1, T2, T3, T4, T5], T6);
-impl_from_request!([T1, T2, T3, T4, T5, T6], T7);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7], T8);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8], T9);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9], T10);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
-impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
-impl_from_request!(
-    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13],
-    T14
-);
-impl_from_request!(
-    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14],
-    T15
-);
-impl_from_request!(
-    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15],
-    T16
-);
+//                 Ok(($($ty,)* $last,))
+//             }
+//         }
+//     };
+// }
+
+// impl_from_request!([], T1);
+// impl_from_request!([T1], T2);
+// impl_from_request!([T1, T2], T3);
+// impl_from_request!([T1, T2, T3], T4);
+// impl_from_request!([T1, T2, T3, T4], T5);
+// impl_from_request!([T1, T2, T3, T4, T5], T6);
+// impl_from_request!([T1, T2, T3, T4, T5, T6], T7);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7], T8);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8], T9);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9], T10);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
+// impl_from_request!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
+// impl_from_request!(
+//     [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13],
+//     T14
+// );
+// impl_from_request!(
+//     [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14],
+//     T15
+// );
+// impl_from_request!(
+//     [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15],
+//     T16
+// );
