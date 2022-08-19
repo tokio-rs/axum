57c09dce7a1f906f53eb896c0c71218b33c506f5
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-08-19 12:25:33 +0200

    examples(customize-extractor-error): Added `with_rejection` comments

--- examples/customize-extractor-error/src/with_rejection.rs
+++ examples/customize-extractor-error/src/with_rejection.rs
@@ -1,20 +1,44 @@
+//! Uses `axum_extra::extract::WithRejection` to transform one rejection into
+//!  another
+//!
+//! + Easy learning curve: `WithRejection` acts as a wrapper for another
+//!   already existing rejection type. You only need to provide a `From` impl
+//!   between the original rejection type and the target rejection. Crates like
+//!   `thiserror` can provide such conversion using derive macros. See
+//!   [`thiserror`]
+//! - Verbose types: types become much larger, which makes them difficult to
+//!   read. Current limitations on type aliasing makes imposible to destructure
+//!   a type alias. See [#1116]
+//!
+//!   
+//! [`thiserror`]: https://crates.io/crates/thiserror
+//! [#1116]: https://github.com/tokio-rs/axum/issues/1116#issuecomment-1186197684
+
 use axum::{extract::rejection::JsonRejection, http::StatusCode, response::IntoResponse, Json};
 use axum_extra::extract::WithRejection;
 use serde_json::{json, Value};
 use thiserror::Error;
 
 pub async fn handler(
+    // `WitRejection` will extract `Json<Value>` from the request. If it fails
+    // `JsonRejection` will be transform into `ApiError` and returned as response
+    // to the client.
+    //
+    // The second constructor argument is not meaningful and can be safely ignored
     WithRejection(Json(value), _): WithRejection<Json<Value>, ApiError>,
 ) -> impl IntoResponse {
     Json(dbg!(value))
 }
 
+// We derive `thiserror::Error`
 #[derive(Debug, Error)]
 pub enum ApiError {
+    // The `#[ƒrom]` attribute generates `From<JsonRejection> for ApiError`
+    // implementation. See `thiserror` docs for more information
     #[error(transparent)]
     JsonExtractorRejection(#[from] JsonRejection),
 }
-
+// We implement `IntoResponse` so ApiError can be used as a response
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
         let payload = json!({
