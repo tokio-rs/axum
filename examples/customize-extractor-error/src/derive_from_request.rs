22c74ed37c7c5cbbb868623e504e013b7e6cc7c5
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-08-19 13:00:38 +0200

    examples(customize-extractor-error): Added `derive_from_request` comments

--- examples/customize-extractor-error/src/derive_from_request.rs
+++ examples/customize-extractor-error/src/derive_from_request.rs
@@ -1,3 +1,17 @@
+//! Uses `axum_macros::FromRequest` to wrap another extractor and customize the
+//! rejection
+//!
+//! + Easy learning curve: Deriving `FromRequest` generates a `FromRequest`
+//!   implementation for your type using another extractor. You only need
+//!   to provide a `From` impl between the original rejection type and the
+//!   target rejection. Crates like [`thiserror`] can provide such conversion
+//!   using derive macros.
+//! - Boilerplate: Requires deriving `FromRequest` for every custom rejection
+//! - There are some known limitations: [FromRequest#known-limitations]
+//!
+//! [`thiserror`]: https://crates.io/crates/thiserror
+//! [FromRequest#known-limitations]: https://docs.rs/axum-macros/*/axum_macros/derive.FromRequest.html#known-limitations
+//!
 use axum::{extract::rejection::JsonRejection, http::StatusCode, response::IntoResponse};
 use axum_macros::FromRequest;
 use serde_json::{json, Value};
@@ -11,12 +25,14 @@ pub async fn handler(Json(value): Json<Value>) -> impl IntoResponse {
 #[from_request(via(axum::Json), rejection(ApiError))]
 pub struct Json<T>(T);
 
+// We create our own rejection type
 #[derive(Debug)]
 pub struct ApiError {
     code: StatusCode,
     message: String,
 }
 
+// We implement `From<JsonRejection> for ApiError`
 impl From<JsonRejection> for ApiError {
     fn from(rejection: JsonRejection) -> Self {
         let code = match rejection {
@@ -32,6 +48,7 @@ impl From<JsonRejection> for ApiError {
     }
 }
 
+// We implement `IntoResponse` so ApiError can be used as a response
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
         let payload = json!({
