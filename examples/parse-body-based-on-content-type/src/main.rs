0a51cb681f517b461fe329e8c297c60ad6af1c97
David Pedersen <david.pdrsn@gmail.com>
2022-08-25 15:43:44 +0200

    fix copy/paste errors

--- examples/parse-body-based-on-content-type/src/main.rs
+++ examples/parse-body-based-on-content-type/src/main.rs
@@ -1,16 +1,9 @@
 //! Provides a RESTful web server managing some Todos.
 //!
-//! API will be:
-//!
-//! - `GET /todos`: return a JSON list of Todos.
-//! - `POST /todos`: create a new Todo.
-//! - `PUT /todos/:id`: update a specific Todo.
-//! - `DELETE /todos/:id`: delete a specific Todo.
-//!
 //! Run with
 //!
 //! ```not_rust
-//! cd examples && cargo run -p example-todos
+//! cd examples && cargo run -p example-parse-body-based-on-content-type
 //! ```
 
 use axum::{
