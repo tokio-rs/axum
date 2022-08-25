e0a6737d5cc74cbeb0d80fb6a21501e2b18a354f
David Pedersen <david.pdrsn@gmail.com>
2022-08-25 15:44:02 +0200

    Update examples/parse-body-based-on-content-type/src/main.rs
    Co-authored-by: Jonas Platte <jplatte+git@posteo.de>

--- examples/parse-body-based-on-content-type/src/main.rs
+++ examples/parse-body-based-on-content-type/src/main.rs
@@ -76,21 +76,19 @@ where
     type Rejection = Response;
 
     async fn from_request(req: Request<B>, _state: &S) -> Result<Self, Self::Rejection> {
-        let content_type = req
-            .headers()
-            .get(CONTENT_TYPE)
-            .and_then(|value| value.to_str().ok())
-            .map(|value| value.to_owned())
-            .ok_or_else(|| StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())?;
+        let content_type_header = req.headers().get(CONTENT_TYPE);
+        let content_type = content_type_header.and_then(|value| value.to_str().ok());
 
-        if content_type.starts_with("application/json") {
-            let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
-            return Ok(Self::Json(payload));
-        }
+        if let Some(content_type) = content_type {
+            if content_type.starts_with("application/json") {
+                let Json(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+                return Ok(Self::Json(payload));
+            }
 
-        if content_type.starts_with("application/x-www-form-urlencoded") {
-            let Form(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
-            return Ok(Self::Form(payload));
+            if content_type.starts_with("application/x-www-form-urlencoded") {
+                let Form(payload) = req.extract().await.map_err(IntoResponse::into_response)?;
+                return Ok(Self::Form(payload));
+            }
         }
 
         Err(StatusCode::UNSUPPORTED_MEDIA_TYPE.into_response())
