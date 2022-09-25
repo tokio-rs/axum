0e4a894c1656bb490232ea93713f065ba8d28107
David Pedersen <david.pdrsn@gmail.com>
2022-09-25 15:26:36 +0200

    Fix formatting of one more macro

--- axum/src/macros.rs
+++ axum/src/macros.rs
@@ -178,6 +178,7 @@ macro_rules! composite_rejection {
     };
 }
 
+#[rustfmt::skip]
 macro_rules! all_the_tuples {
     ($name:ident) => {
         $name!([], T1);
@@ -193,17 +194,8 @@ macro_rules! all_the_tuples {
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
         $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
-        $name!(
-            [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13],
-            T14
-        );
-        $name!(
-            [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14],
-            T15
-        );
-        $name!(
-            [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15],
-            T16
-        );
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13], T14);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14], T15);
+        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15], T16);
     };
 }
