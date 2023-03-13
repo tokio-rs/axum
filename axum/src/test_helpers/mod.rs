cba7c8430996c749dd2c6cbe3725c174601aa81a
David Pedersen <david.pdrsn@gmail.com>
2023-03-13 22:55:00 +0100

    Add `Body::from_stream`
    This replaces `StreamBody` with `Body::from_stream`.
    
    Requires `axum-core` to depend on `pin-project-lite` but that is already
    pulled in by `pin-project-lite`.

--- axum/src/test_helpers/mod.rs
+++ axum/src/test_helpers/mod.rs
@@ -7,6 +7,5 @@ pub(crate) use self::test_client::*;
 
 pub(crate) fn assert_send<T: Send>() {}
 pub(crate) fn assert_sync<T: Sync>() {}
-pub(crate) fn assert_unpin<T: Unpin>() {}
 
 pub(crate) struct NotSendSync(*const ());
