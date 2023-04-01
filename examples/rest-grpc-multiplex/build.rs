2e182b0095ca3dfed133fb32d6594765971f80fc
David Pedersen <david.pdrsn@gmail.com>
2023-04-01 12:15:56 +0200

    Update `rest-grpc-multiplex` example to include reflection

--- examples/rest-grpc-multiplex/build.rs
+++ examples/rest-grpc-multiplex/build.rs
@@ -1,3 +1,10 @@
+use std::{env, path::PathBuf};
+
 fn main() {
-    tonic_build::compile_protos("proto/helloworld.proto").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
+        .compile(&["proto/helloworld.proto"], &["/proto"])
+        .unwrap();
 }
