b0eb7a24bc62c76d59d2a98117c27a4bdb11a34a
Matt Fellenz <matt@felle.nz>
2023-05-03 14:17:18 -0700

    Fix localhost vs 0.0.0.0 discrepancy (#1984)

--- axum/src/lib.rs
+++ axum/src/lib.rs
@@ -53,7 +53,7 @@
 //!     // build our application with a single route
 //!     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 //!
-//!     // run it on localhost:3000
+//!     // run our app with hyper, listening globally on port 3000
 //!     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 //!     axum::serve(listener, app).await.unwrap();
 //! }
