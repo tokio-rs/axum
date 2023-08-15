e24a8494499e9389f1aa596b9ae8b9535cf360a2
Jonas Platte <jplatte+git@posteo.de>
2023-08-15 17:26:53 +0200

    Fix documentation for MethodRouter::into_make_service (#2176)

--- axum/src/routing/method_routing.rs
+++ axum/src/routing/method_routing.rs
@@ -636,9 +636,10 @@ impl<B> MethodRouter<(), B, Infallible>
 where
     B: HttpBody + Send + 'static,
 {
-    /// Convert the handler into a [`MakeService`].
+    /// Convert the router into a [`MakeService`].
     ///
-    /// This allows you to serve a single handler if you don't need any routing:
+    /// This allows you to serve a single `MethodRouter` if you don't need any
+    /// routing based on the path:
     ///
     /// ```rust
     /// use axum::{
