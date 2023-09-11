02362bf28d909639f2de04240ae0210a30530dd1
David Pedersen <david.pdrsn@gmail.com>
2023-09-11 14:02:07 +0200

    format

--- examples/customize-extractor-error/src/derive_from_request.rs
+++ examples/customize-extractor-error/src/derive_from_request.rs
@@ -28,7 +28,7 @@ pub async fn handler(Json(value): Json<Value>) -> impl IntoResponse {
 pub struct Json<T>(T);
 
 // We implement `IntoResponse` for our extractor so it can be used as a response
-impl <T: Serialize> IntoResponse for Json<T> {
+impl<T: Serialize> IntoResponse for Json<T> {
     fn into_response(self) -> axum::response::Response {
         let Self(value) = self;
         axum::Json(value).into_response()
