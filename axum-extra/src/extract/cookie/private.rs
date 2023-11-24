e3d34bb553d50fe6989eae957007b38a8e3cc085
Michael Seele <mseele@gmail.com>
2023-11-24 21:39:14 +0100

    axum-extra: Use impl Into<Cookie> (#2348)

--- axum-extra/src/extract/cookie/private.rs
+++ axum-extra/src/extract/cookie/private.rs
@@ -220,7 +220,7 @@ impl<K> PrivateCookieJar<K> {
     /// }
     /// ```
     #[must_use]
-    pub fn remove(mut self, cookie: Cookie<'static>) -> Self {
+    pub fn remove<C: Into<Cookie<'static>>>(mut self, cookie: C) -> Self {
         self.private_jar_mut().remove(cookie);
         self
     }
@@ -241,7 +241,7 @@ impl<K> PrivateCookieJar<K> {
     /// ```
     #[must_use]
     #[allow(clippy::should_implement_trait)]
-    pub fn add(mut self, cookie: Cookie<'static>) -> Self {
+    pub fn add<C: Into<Cookie<'static>>>(mut self, cookie: C) -> Self {
         self.private_jar_mut().add(cookie);
         self
     }
