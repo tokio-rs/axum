c4f8d68ec6c8f81157d3eff33c5834a4c663efe8
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-11-29 01:14:01 +0300

    Added body::to_bytes

--- axum/src/body/mod.rs
+++ axum/src/body/mod.rs
@@ -8,3 +8,17 @@ pub use bytes::Bytes;
 
 #[doc(inline)]
 pub use axum_core::body::Body;
+
+use http_body_util::BodyExt;
+
+/// Converts [`Body`] into [`Bytes`].
+/// ```rust
+/// # use axum::body::to_bytes;
+/// async fn foo() {
+///     let body = Body::from(vec![1, 2, 3]);
+///     assert_eq!(&to_bytes(body).await.unwrap()[..], &[1, 2, 3]);
+/// }
+/// ```
+pub async fn to_bytes(body: Body) -> Result<Bytes, axum_core::Error> {
+    body.collect().await.map(|col| col.to_bytes())
+}
