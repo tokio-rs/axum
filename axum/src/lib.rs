6dd5d0be9dee99f3c6c0b5be5e53599433f7aea3
Keita Nonaka <iKonnyaku40@gmail.com>
2023-11-28 03:00:57 -0800

    fix: back to forbid `unsafe` (#2365)

--- axum/src/lib.rs
+++ axum/src/lib.rs
@@ -413,9 +413,7 @@
 )]
 #![deny(unreachable_pub)]
 #![allow(elided_lifetimes_in_paths, clippy::type_complexity)]
-// can't be `forbid` since we've vendored code from hyper-util that contains `unsafe`
-// when hyper-util is on crates.io we can stop vendoring it and go back to `forbid`
-#![deny(unsafe_code)]
+#![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
 #![cfg_attr(test, allow(clippy::float_cmp))]
 #![cfg_attr(not(test), warn(clippy::print_stdout, clippy::dbg_macro))]
