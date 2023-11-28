b835b8429826af363658b6d8965470efe062f1d1
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2023-11-29 02:19:34 +0300

    Limit maximum size of the body && tweaked doc test

--- axum/src/body/mod.rs
+++ axum/src/body/mod.rs
@@ -9,16 +9,27 @@ pub use bytes::Bytes;
 #[doc(inline)]
 pub use axum_core::body::Body;
 
-use http_body_util::BodyExt;
+use http_body_util::{BodyExt, Limited};
 
-/// Converts [`Body`] into [`Bytes`].
+/// Converts [`Body`] into [`Bytes`] and limits the maximum size of the body.
+///
+/// # Example
+///
 /// ```rust
-/// # use axum::body::{to_bytes, Body};
-/// async fn foo() {
-///     let body = Body::from(vec![1, 2, 3]);
-///     assert_eq!(&to_bytes(body).await.unwrap()[..], &[1, 2, 3]);
-/// }
+/// use axum::body::{to_bytes, Body};
+///
+/// # async fn foo() -> Result<(), axum_core::Error> {
+/// let body = Body::from(vec![1, 2, 3]);
+/// // Use `usize::MAX` if you don't care about the maximum size.
+/// let bytes = to_bytes(body, usize::MAX).await?;
+/// assert_eq!(&bytes[..], &[1, 2, 3]);
+/// # Ok(())
+/// # }
 /// ```
-pub async fn to_bytes(body: Body) -> Result<Bytes, axum_core::Error> {
-    body.collect().await.map(|col| col.to_bytes())
+pub async fn to_bytes(body: Body, limit: usize) -> Result<Bytes, axum_core::Error> {
+    Limited::new(body, limit)
+        .collect()
+        .await
+        .map(|col| col.to_bytes())
+        .map_err(axum_core::Error::new)
 }
