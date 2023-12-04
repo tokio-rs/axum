2b486ea174b263e4335490794637ea50eb04765c
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-12-04 18:04:07 +0000

    docs: Typo correction (#2405)

--- axum-extra/src/lib.rs
+++ axum-extra/src/lib.rs
@@ -17,7 +17,7 @@
 //! `erased-json` | Enables the `ErasedJson` response | No
 //! `form` | Enables the `Form` extractor | No
 //! `json-lines` | Enables the `JsonLines` extractor and response | No
-//! `multipart` | Enables the `Multpart` extractor | No
+//! `multipart` | Enables the `Multipart` extractor | No
 //! `protobuf` | Enables the `Protobuf` extractor and response | No
 //! `query` | Enables the `Query` extractor | No
 //! `typed-routing` | Enables the `TypedPath` routing utilities | No
