a862c2202a74b5739052cf3f731a81a2a1baaf7e
Yann Simon <yann.simon@commercetools.com>
2024-02-29 17:21:04 +0100

    test request body decompression

--- examples/compression/src/main.rs
+++ examples/compression/src/main.rs
@@ -14,11 +14,7 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app: Router = Router::new().route("/", post(root)).layer(
-        ServiceBuilder::new()
-            .layer(RequestDecompressionLayer::new())
-            .layer(CompressionLayer::new()),
-    );
+    let app: Router = app();
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -27,6 +23,162 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+fn app() -> Router {
+    Router::new().route("/", post(root)).layer(
+        ServiceBuilder::new()
+            .layer(RequestDecompressionLayer::new())
+            .layer(CompressionLayer::new()),
+    )
+}
+
 async fn root(Json(value): Json<Value>) -> Json<Value> {
     Json(value)
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_json_diff::assert_json_eq;
+    use axum::{
+        body::{Body, Bytes},
+        response::Response,
+    };
+    use brotli::enc::BrotliEncoderParams;
+    use flate2::{write::GzEncoder, Compression};
+    use http::StatusCode;
+    use serde_json::{json, Value};
+    use std::io::Write;
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn handle_uncompressed_request_bodies() {
+        // Given
+
+        let body = serde_json::to_vec(&json()).unwrap();
+
+        let compressed_request = http::Request::post("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .body(Body::from(body))
+            .unwrap();
+
+        // When
+
+        let response = app().oneshot(compressed_request).await.unwrap();
+
+        // Then
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_json_eq!(json_from_response(response).await, json());
+    }
+
+    #[tokio::test]
+    async fn decompress_gzip_request_bodies() {
+        // Given
+
+        let body = compress_gzip(&json());
+
+        let compressed_request = http::Request::post("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .header("Content-Encoding", "gzip")
+            .body(Body::from(body))
+            .unwrap();
+
+        // When
+
+        let response = app().oneshot(compressed_request).await.unwrap();
+
+        // Then
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_json_eq!(json_from_response(response).await, json());
+    }
+
+    #[tokio::test]
+    async fn decompress_br_request_bodies() {
+        // Given
+
+        let body = compress_br(&json());
+
+        let compressed_request = http::Request::post("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .header("Content-Encoding", "br")
+            .body(Body::from(body))
+            .unwrap();
+
+        // When
+
+        let response = app().oneshot(compressed_request).await.unwrap();
+
+        // Then
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_json_eq!(json_from_response(response).await, json());
+    }
+
+    #[tokio::test]
+    async fn decompress_zstd_request_bodies() {
+        // Given
+
+        let body = compress_zstd(&json());
+
+        let compressed_request = http::Request::post("/")
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .header("Content-Encoding", "zstd")
+            .body(Body::from(body))
+            .unwrap();
+
+        // When
+
+        let response = app().oneshot(compressed_request).await.unwrap();
+
+        // Then
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_json_eq!(json_from_response(response).await, json());
+    }
+
+    fn json() -> Value {
+        json!({
+          "name": "foo",
+          "mainProduct": {
+            "typeId": "product",
+            "id": "p1"
+          },
+        })
+    }
+
+    async fn json_from_response(response: Response) -> Value {
+        let body = axum::body::to_bytes(response.into_body(), usize::MAX)
+            .await
+            .unwrap();
+        body_as_json(body)
+    }
+
+    fn body_as_json(body: Bytes) -> Value {
+        serde_json::from_slice(body.as_ref()).unwrap()
+    }
+
+    fn compress_gzip(json: &Value) -> Vec<u8> {
+        let request_body = serde_json::to_vec(&json).unwrap();
+
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+        encoder.write_all(&request_body).unwrap();
+        encoder.finish().unwrap()
+    }
+
+    fn compress_br(json: &Value) -> Vec<u8> {
+        let request_body = serde_json::to_vec(&json).unwrap();
+        let mut result = Vec::new();
+
+        let params = BrotliEncoderParams::default();
+        let _ = brotli::enc::BrotliCompress(&mut &request_body[..], &mut result, &params).unwrap();
+
+        result
+    }
+
+    fn compress_zstd(json: &Value) -> Vec<u8> {
+        let request_body = serde_json::to_vec(&json).unwrap();
+        zstd::stream::encode_all(std::io::Cursor::new(request_body), 4).unwrap()
+    }
 }
