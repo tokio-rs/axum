b6b203b3065e4005bda01efac8429176da055ae2
Vigith Maurice <vigith@gmail.com>
2024-03-02 12:51:18 -0800

    fix typo in prometheus_metrics_example (#2627)
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

--- examples/prometheus-metrics/src/main.rs
+++ examples/prometheus-metrics/src/main.rs
@@ -64,7 +64,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_todos=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "example_prometheus_metrics=debug,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
