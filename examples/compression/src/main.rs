fda6f0914cdd07f83bb10179210eca1994b1212a
Yann Simon <yann.simon.fr@gmail.com>
2024-05-02 21:48:41 +0200

    fix log
    Co-authored-by: Jonas Platte <jplatte+git@posteo.de>

--- examples/compression/src/main.rs
+++ examples/compression/src/main.rs
@@ -9,7 +9,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "compression=trace".into()),
+                .unwrap_or_else(|_| "example-compression=trace".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
