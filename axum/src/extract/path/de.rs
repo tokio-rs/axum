4179d11797bd8880c74a306b7e05e9d0f081ca1d
Hytak <lhc.holten@gmail.com>
2024-09-24 19:17:16 +0200

    Stricter path deserialization for tuples and tuple structs (#2931)

--- axum/src/extract/path/de.rs
+++ axum/src/extract/path/de.rs
@@ -140,7 +140,7 @@ impl<'de> Deserializer<'de> for PathDeserializer<'de> {
     where
         V: Visitor<'de>,
     {
-        if self.url_params.len() < len {
+        if self.url_params.len() != len {
             return Err(PathDeserializationError::wrong_number_of_parameters()
                 .got(self.url_params.len())
                 .expected(len));
@@ -160,7 +160,7 @@ impl<'de> Deserializer<'de> for PathDeserializer<'de> {
     where
         V: Visitor<'de>,
     {
-        if self.url_params.len() < len {
+        if self.url_params.len() != len {
             return Err(PathDeserializationError::wrong_number_of_parameters()
                 .got(self.url_params.len())
                 .expected(len));
@@ -774,20 +774,6 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_tuple_ignoring_additional_fields() {
-        let url_params = create_url_params(vec![
-            ("a", "abc"),
-            ("b", "true"),
-            ("c", "1"),
-            ("d", "false"),
-        ]);
-        assert_eq!(
-            <(&str, bool, u32)>::deserialize(PathDeserializer::new(&url_params)).unwrap(),
-            ("abc", true, 1)
-        );
-    }
-
-    #[test]
     fn test_parse_map() {
         let url_params = create_url_params(vec![("a", "1"), ("b", "true"), ("c", "abc")]);
         assert_eq!(
@@ -811,6 +797,18 @@ mod tests {
                 .kind;
             assert_eq!(actual_error_kind, $expected_error_kind);
         };
+    }
+
+    #[test]
+    fn test_parse_tuple_too_many_fields() {
+        test_parse_error!(
+            vec![("a", "abc"), ("b", "true"), ("c", "1"), ("d", "false"),],
+            (&str, bool, u32),
+            ErrorKind::WrongNumberOfParameters {
+                got: 4,
+                expected: 3,
+            }
+        );
     }
 
     #[test]
