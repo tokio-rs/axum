19b13bb043a695b18a8e7e7f75fcba2490c5d0fe
Leon Lux <leonlux@icloud.com>
2024-09-29 17:50:40 +0200

    change method_not_allowed_fallback to use default_fallback

--- axum/src/routing/path_router.rs
+++ axum/src/routing/path_router.rs
@@ -89,7 +89,7 @@ where
     {
         for (_, endpoint) in self.routes.iter_mut() {
             if let Endpoint::MethodRouter(rt) = endpoint {
-                *rt = rt.clone().fallback(handler.clone());
+                *rt = rt.clone().default_fallback(handler.clone());
             }
         }
     }
