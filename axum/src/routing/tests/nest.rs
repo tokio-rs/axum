a34b73eabcce98e842e5202034d1f51d2a0928c1
Jonas Platte <jplatte+git@posteo.de>
2024-10-19 22:18:01 +0200

    Upgrade matchit

--- axum/src/routing/tests/nest.rs
+++ axum/src/routing/tests/nest.rs
@@ -228,7 +228,7 @@ async fn nested_multiple_routes() {
 }
 
 #[test]
-#[should_panic = "Invalid route \"/\": insertion failed due to conflict with previously registered route: /"]
+#[should_panic = r#"Invalid route "/": Insertion failed due to conflict with previously registered route: /"#]
 fn nested_service_at_root_with_other_routes() {
     let _: Router = Router::new()
         .nest_service("/", Router::new().route("/users", get(|| async {})))
