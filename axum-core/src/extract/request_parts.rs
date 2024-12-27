5b87a5b6fe41566c1bc5cbc6575aa61531b473d7
Tobias Bieniek <tobias@bieniek.cloud>
2024-12-27 13:46:39 +0100

    axum: Move `request_parts` tests into `axum-core`
    This is where the extractor is implemented, so it would make sense to colocate the tests there too.

--- axum-core/src/extract/request_parts.rs
+++ axum-core/src/extract/request_parts.rs
@@ -166,3 +166,28 @@ where
         Ok(req.into_body())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{extract::Extension, routing::get, test_helpers::*, Router};
+    use http::{Method, StatusCode};
+
+    #[crate::test]
+    async fn extract_request_parts() {
+        #[derive(Clone)]
+        struct Ext;
+
+        async fn handler(parts: http::request::Parts) {
+            assert_eq!(parts.method, Method::GET);
+            assert_eq!(parts.uri, "/");
+            assert_eq!(parts.version, http::Version::HTTP_11);
+            assert_eq!(parts.headers["x-foo"], "123");
+            parts.extensions.get::<Ext>().unwrap();
+        }
+
+        let client = TestClient::new(Router::new().route("/", get(handler)).layer(Extension(Ext)));
+
+        let res = client.get("/").header("x-foo", "123").await;
+        assert_eq!(res.status(), StatusCode::OK);
+    }
+}
