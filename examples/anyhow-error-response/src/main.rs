ac7732abd391648f3409d57ee61c6c561df9edcc
Gábor Szabó <gabor@szabgab.com>
2025-03-18 11:53:00 +0200

    [test] the anyhow-error-response example (#3274)

--- examples/anyhow-error-response/src/main.rs
+++ examples/anyhow-error-response/src/main.rs
@@ -13,7 +13,7 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = app();
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -45,6 +45,10 @@ impl IntoResponse for AppError {
     }
 }
 
+fn app() -> Router {
+    Router::new().route("/", get(handler))
+}
+
 // This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
 // `Result<_, AppError>`. That way you don't need to do that manually.
 impl<E> From<E> for AppError
@@ -53,5 +57,28 @@ where
 {
     fn from(err: E) -> Self {
         Self(err.into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{body::Body, http::Request, http::StatusCode};
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn test_main_page() {
+        let response = app()
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        let body = response.into_body();
+        let bytes = body.collect().await.unwrap().to_bytes();
+        let html = String::from_utf8(bytes.to_vec()).unwrap();
+
+        assert_eq!(html, "Something went wrong: it failed!");
     }
 }
