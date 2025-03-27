4667181b9f0e53b99c64f2627beec027b612c902
Joern Barthel <joern@barthel.digital>
2025-03-27 16:39:57 +0100

    Simplified closure example
    The example is now concrete, without abstract identifiers and a without a macro.

--- examples/closures/src/main.rs
+++ examples/closures/src/main.rs
@@ -1,4 +1,10 @@
-//! Example for using associated methods as handlers or middleware.
+//! Example for using associated methods as handlers and middleware.
+//!
+//! In situations where applications want to encapsulate state (e.g. auth secrets) it
+//! can be useful to create routes and middleware directly on specific structs (as opposed to
+//! handling everything in a central application state).
+//!
+//! This example demonstrates how do that using closures.  
 //!
 //! Run with
 //!
@@ -7,90 +13,76 @@
 //! ```
 
 use axum::{
-    extract::{Query, Request, State},
+    extract::Request,
+    http::StatusCode,
     middleware::{from_fn, Next},
     response::IntoResponse,
     routing::get,
     Router,
 };
-use std::{
-    collections::HashMap,
-    sync::{atomic::AtomicU64, Arc},
-};
+use std::{collections::HashMap, sync::Arc};
 
-struct AppState {
-    version: String,
+#[derive(Clone)]
+struct Username(String);
+
+pub struct Auth {
+    database: HashMap<String, String>,
 }
 
-type S = Arc<AppState>;
-
-struct Bar {
-    id: AtomicU64,
-    prefix: String,
-}
-
-struct Foo {
-    bar: Bar,
-}
-
-impl Foo {
-    async fn a(self: Arc<Self>, q: Query<HashMap<String, String>>) -> impl IntoResponse {
-        let msg = q.get("msg").cloned().unwrap_or("world".to_string());
-        format!("{} {}", self.bar.prefix, msg)
-    }
-
-    async fn b(self: Arc<Self>, state: State<S>, mut req: Request) -> impl IntoResponse {
-        let id = req.extensions_mut().remove::<u64>();
-
-        if let Some(id) = id {
-            format!("{}: id {}", state.version, id)
+impl Auth {
+    /// read_username returns the username if it has been previously set by the middleware.
+    /// Otherwise it returns 401.
+    async fn read_username(self: Arc<Self>, req: Request) -> Result<impl IntoResponse, StatusCode> {
+        if let Some(username) = req.extensions().get::<Username>() {
+            Ok(format!("hello {}", username.0))
         } else {
-            format!("{}: no id", state.version)
+            Err(StatusCode::UNAUTHORIZED)
         }
     }
 
-    async fn c(self: Arc<Self>, mut req: Request, next: Next) -> impl IntoResponse {
-        let id = self
-            .bar
-            .id
-            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-
-        req.extensions_mut().insert(id);
+    /// middleware checks if the request has a valid secret and sets the username (if available
+    /// in the database) as a request extension).
+    async fn middleware(self: Arc<Self>, mut req: Request, next: Next) -> impl IntoResponse {
+        if let Some(secret) = req.headers().get("X-Auth-Secret") {
+            if let Some(username) = self.database.get(secret.to_str().unwrap()) {
+                req.extensions_mut().insert(Username(username.clone()));
+            }
+        }
 
         next.run(req).await
     }
 }
 
-macro_rules! c {
-    ($arc:expr, |$($param:ident),*| $method:ident) => {{
-        let s = Arc::clone(&$arc);
-        move |$($param),*| {
-            let s = Arc::clone(&s); // only needed for middleware
-            async move { s.$method($($param),*).await }
-        }
-    }};
-}
-
 #[tokio::main]
 async fn main() {
-    let state = Arc::new(AppState {
-        version: "v1".to_string(),
-    });
-
-    let f = Foo {
-        bar: Bar {
-            id: AtomicU64::new(0),
-            prefix: "Hello".to_string(),
+    let auth = Auth {
+        database: {
+            let mut db = HashMap::new();
+            db.insert("open sesame".to_string(), "admin".to_string());
+            db
         },
     };
 
-    let f = Arc::new(f);
+    // wrap auth in an Arc smart pointer to share it between threads
+    let auth = Arc::new(auth);
 
     let app = Router::new()
-        .route("/a", get(c!(f, |q| a)))
-        .route("/b", get(c!(f, |s, q| b)))
-        .layer(from_fn(c!(f, |r, n| c)))
-        .with_state(state);
+        // route /username to the auth instance behind the smart pointer
+        .route(
+            "/username",
+            get({
+                let auth = Arc::clone(&auth);
+                |req| async { auth.read_username(req).await }
+            }),
+        )
+        // configure a middleware from the auth instance behind the smart pointer
+        .layer(from_fn({
+            let auth = Arc::clone(&auth);
+            move |req, next| {
+                let auth = Arc::clone(&auth);
+                async move { auth.middleware(req, next).await }
+            }
+        }));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
