tokio-rs/axum

commit f978792608b223e03b764348537d7a205d6dd2bc

refactor: don't use wildcard imports
Wildcard imports can pollute the namespace, and can lead to confusing
error messages or even unexpected behaviour. This commit uses explicit
imports instead. Although slightly more verbose, it can save a lot of
debugging efforts in the future.

Exceptions to this rule are prelude imports, imports of `pub use`
`re-exports`, and `super::*` imports in tests.

This fixes the `wildcard_imports` clippy lint.

axum/src/extract/ws.rs
@@ -90,7 +90,11 @@
 //!
 //! [`StreamExt::split`]: https://docs.rs/futures/0.3.17/futures/stream/trait.StreamExt.html#method.split
 
-use self::rejection::*;
+use self::rejection::{
+    ConnectionNotUpgradable, InvalidConnectionHeader, InvalidProtocolPseudoheader,
+    InvalidUpgradeHeader, InvalidWebSocketVersionHeader, MethodNotConnect, MethodNotGet,
+    WebSocketKeyHeaderMissing, WebSocketUpgradeRejection,
+};
 use super::FromRequestParts;
 use crate::{body::Bytes, response::Response, Error};
 use axum_core::body::Body;

Cargo.toml
@@ -58,6 +58,7 @@ map_unwrap_or = "warn"
 semicolon_if_nothing_returned = "warn"
 manual_assert = "warn"
 ignored_unit_patterns = "warn"
+wildcard_imports = "warn"
 
 # configuration for https://github.com/crate-ci/typos
 [workspace.metadata.typos.default.extend-identifiers]

axum-core/src/extract/request_parts.rs
@@ -1,4 +1,7 @@
-use super::{rejection::*, FromRequest, FromRequestParts, Request};
+use super::{
+    rejection::{BytesRejection, FailedToBufferBody, InvalidUtf8, StringRejection},
+    FromRequest, FromRequestParts, Request,
+};
 use crate::{body::Body, RequestExt};
 use bytes::{BufMut, Bytes, BytesMut};
 use http::{request::Parts, Extensions, HeaderMap, Method, Uri, Version};

axum/src/extension.rs
@@ -1,4 +1,7 @@
-use crate::{extract::rejection::*, response::IntoResponseParts};
+use crate::{
+    extract::rejection::{ExtensionRejection, MissingExtension},
+    response::IntoResponseParts,
+};
 use axum_core::extract::OptionalFromRequestParts;
 use axum_core::{
     extract::FromRequestParts,

axum/src/extract/matched_path.rs
@@ -1,4 +1,7 @@
-use super::{rejection::*, FromRequestParts};
+use super::{
+    rejection::{MatchedPathMissing, MatchedPathRejection},
+    FromRequestParts,
+};
 use crate::routing::{RouteId, NEST_TAIL_PARAM_CAPTURE};
 use axum_core::extract::OptionalFromRequestParts;
 use http::request::Parts;

axum/src/extract/path/mod.rs
@@ -4,7 +4,10 @@
 mod de;
 
 use crate::{
-    extract::{rejection::*, FromRequestParts},
+    extract::{
+        rejection::{MissingPathParams, PathRejection, RawPathParamsRejection},
+        FromRequestParts,
+    },
     routing::url_params::UrlParams,
     util::PercentDecodedStr,
 };

axum/src/extract/query.rs
@@ -1,4 +1,7 @@
-use super::{rejection::*, FromRequestParts};
+use super::{
+    rejection::{FailedToDeserializeQueryString, QueryRejection},
+    FromRequestParts,
+};
 use http::{request::Parts, Uri};
 use serde::de::DeserializeOwned;
 

axum/src/form.rs
@@ -1,5 +1,10 @@
 use crate::extract::Request;
-use crate::extract::{rejection::*, FromRequest, RawForm};
+use crate::extract::{
+    rejection::{
+        FailedToDeserializeForm, FailedToDeserializeFormBody, FormRejection, RawFormRejection,
+    },
+    FromRequest, RawForm,
+};
 use axum_core::response::{IntoResponse, Response};
 use axum_core::RequestExt;
 use http::header::CONTENT_TYPE;
@@ -130,6 +135,7 @@ axum_core::__impl_deref!(Form);
 #[cfg(test)]
 mod tests {
     use crate::{
+        extract::rejection::InvalidFormContentType,
         routing::{on, MethodFilter},
         test_helpers::TestClient,
         Router,

commit 59908042d7317e81e7b791e5391b41bc47c4a14f

fix: respect feature flags in the imports
f978792608b223e03b764348537d7a205d6dd2bc caused a compilation warning to
appear when compiling without the `http2` feature flag. This commit
fixes that by only importing `InvalidProtocolPseudoheader` when the
`http2` feature is enabled.

axum/src/extract/ws.rs
@@ -91,10 +91,12 @@
 //! [`StreamExt::split`]: https://docs.rs/futures/0.3.17/futures/stream/trait.StreamExt.html#method.split
 
 use self::rejection::{
-    ConnectionNotUpgradable, InvalidConnectionHeader, InvalidProtocolPseudoheader,
+    ConnectionNotUpgradable, InvalidConnectionHeader,
     InvalidUpgradeHeader, InvalidWebSocketVersionHeader, MethodNotConnect, MethodNotGet,
     WebSocketKeyHeaderMissing, WebSocketUpgradeRejection,
 };
+#[cfg(feature = "http2")]
+use self::rejection::InvalidProtocolPseudoheader;
 use super::FromRequestParts;
 use crate::{body::Bytes, response::Response, Error};
 use axum_core::body::Body;