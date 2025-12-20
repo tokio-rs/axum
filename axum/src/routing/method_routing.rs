64c83ead874806ef949772a8b7eacac935a1bbae
Jonas Platte <jplatte+git@posteo.de>
2025-12-20 11:31:53 +0100

    Add missing semicolon

--- axum/src/routing/method_routing.rs
+++ axum/src/routing/method_routing.rs
@@ -839,7 +839,7 @@ where
                     panic!(
                         "Overlapping method route. Cannot add two method routes that both handle \
                          `{method_name}`",
-                    )
+                    );
                 }
                 *out = endpoint.clone();
                 for method in methods {
