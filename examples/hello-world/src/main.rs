445e7379b4bba108492eb219db061e9bd9c96341
FarrosFR <56761912+farrosfr@users.noreply.github.com>
2026-05-12 22:48:10 +0700

    Add test coverage for hello-world example

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -9,7 +9,7 @@ use axum::{response::Html, routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = app();
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -19,6 +19,36 @@ async fn main() {
     axum::serve(listener, app).await;
 }
 
+fn app() -> Router {
+    Router::new().route("/", get(handler))
+}
+
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn get_root() {
+        let response = app()
+            .oneshot(Request::get("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body = std::str::from_utf8(&body).unwrap();
+
+        assert_eq!(body, "<h1>Hello, World!</h1>");
+    }
 }
