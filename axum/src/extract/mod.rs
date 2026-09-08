2d0d5987690bec4b25bd585fc516a1b6910045fa
stack3mpty <58629098+stack3mpty@users.noreply.github.com>
2026-09-08 16:00:24 +0800

    Export InnerMatchedPath

--- axum/src/extract/mod.rs
+++ axum/src/extract/mod.rs
@@ -10,6 +10,8 @@ pub mod rejection;
 #[cfg(feature = "ws")]
 pub mod ws;
 
+#[cfg(feature = "matched-path")]
+mod inner_matched_path;
 pub(crate) mod nested_path;
 #[cfg(feature = "original-uri")]
 mod original_uri;
@@ -58,7 +60,7 @@ pub(crate) mod matched_path;
 
 #[cfg(feature = "matched-path")]
 #[doc(inline)]
-pub use self::matched_path::MatchedPath;
+pub use self::{inner_matched_path::InnerMatchedPath, matched_path::MatchedPath};
 
 #[cfg(feature = "multipart")]
 pub mod multipart;
